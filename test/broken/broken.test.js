544d82fa9d3c06a6e7626e20b15fcb9594baf9ae
mendlik <mendelski.pawel@gmail.com>
2015-03-01 22:54:36 +0100

    Fixed test. Removed absolute path from assertion.

--- test/broken/broken.test.js
+++ test/broken/broken.test.js
@@ -9,9 +9,8 @@ describe('Error test: swig-loader', function() {
 	it('should throw nicely formatted error', function(done) {
 		load(path.resolve(__dirname, 'template.js'), function(err, mod) {
 			expect(err).to.exist;
-			expect(err.message).to.contain('Could not resolve swig template. ' +
-				'Cause: Error: Unexpected tag "xyz asd" on line 1 in file ' +
-				'/media/storage/Development/coditorium/code/nodejs/libs/nodejs-swig-loader/test/broken/template.html.');
+			expect(err.message).to.contain('Could not resolve swig template. Cause: Error: Unexpected tag "xyz asd" on line 1 in file ');
+			expect(err.message).to.contain('test/broken/template.html.');
 			expect(mod).to.not.exist;
 			done();
 		});
